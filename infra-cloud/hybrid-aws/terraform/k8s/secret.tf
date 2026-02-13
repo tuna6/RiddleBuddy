bdf68d7f7456c8cc3d9254358b9ee829c43eeb31
tuna6 <nguyen_tu@outlook.com>
2026-02-13 18:39:48 +0700

    remove the double-encoded in secret since kubernetes secret v1 auto encoding the data

--- infra-cloud/hybrid-aws/terraform/k8s/secret.tf
+++ infra-cloud/hybrid-aws/terraform/k8s/secret.tf
@@ -5,10 +5,10 @@ resource "kubernetes_secret_v1" "otel_aws_creds" {
   }
 
   data = {
-    AWS_ACCESS_KEY_ID     = base64encode(var.aws_access_key)
-    AWS_SECRET_ACCESS_KEY = base64encode(var.aws_secret_key)
-    AWS_REGION = base64encode(var.aws_region)
-    AMP_WORKSPACE_ID= base64encode(data.terraform_remote_state.aws.outputs.amp_workspace_id)
+    AWS_ACCESS_KEY_ID     = var.aws_access_key
+    AWS_SECRET_ACCESS_KEY = var.aws_secret_key
+    AWS_REGION = var.aws_region
+    AMP_WORKSPACE_ID= data.terraform_remote_state.aws.outputs.amp_workspace_id
   }
 
   type = "Opaque"
